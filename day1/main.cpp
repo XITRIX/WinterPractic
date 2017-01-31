85b3d783c88a4085c0bf2b244ddf69d3ddd32908
Xitrix <xitrix@bk.ru>
2017-01-31 17:59:23 +0300

    first commit

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,9 +1,6 @@
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include "BTree.h"
 
-using namespace cv;
 using namespace std;
 
 int main( int argc, char** argv)
